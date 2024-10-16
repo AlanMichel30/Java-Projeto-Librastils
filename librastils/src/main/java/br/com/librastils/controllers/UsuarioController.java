0d047c36808df057f6fd658efb96a496afbc41f6
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-10-15 23:57:25 -0300

    Update UsuarioController.java

--- librastils/src/main/java/br/com/librastils/controllers/UsuarioController.java
+++ librastils/src/main/java/br/com/librastils/controllers/UsuarioController.java
@@ -12,18 +12,36 @@ import java.util.List;
 public class UsuarioController {
     private final UsuarioService service;
 
-    public  UsuarioController(UsuarioService service){
+    public UsuarioController(UsuarioService service) {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario){
+    @PostMapping("/usuarios")
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
         var aux = service.criar(usuario);
         return ResponseEntity.ok(aux);
     }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> buscarTodos(){
-        return  ResponseEntity.ok(service.buscarTodos());
+    public ResponseEntity<List<Usuario>> buscarTodos() {
+        return ResponseEntity.ok(service.buscarTodos());
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Usuario> buscar(@PathVariable String cpf) {
+        var aux = service.buscarUsuarioPorCpf(cpf);
+        return ResponseEntity.ok(aux);
+    }
+
+    @PutMapping("/{cpf}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable String cpf, @RequestBody Usuario usuario) {
+        var aux = service.update(usuario, cpf);
+        return ResponseEntity.ok(aux);
+    }
+
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Void> apagar(@PathVariable String cpf) {
+        service.delete(cpf);
+        return ResponseEntity.noContent().build();
     }
 }
